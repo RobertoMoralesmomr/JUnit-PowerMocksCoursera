d3e5618b949079f7fa0da34da02af68629ce3784
José Roberto Mociño Morales <99136589+RobertoMoralesmomr@users.noreply.github.com>
2025-05-28 19:55:51 +0000

    Primer commit

--- claseMain.java
+++ claseMain.java
@@ -1,9 +1,13 @@
-public class claseMain(){
+public class claseMain{
   public static void main (String[]args){
-    public static int suma(int a, int b){
-      return a+b;
-}
+    System.out.println("Hols perros desde mi iPad");
 
-} 
+    System.out.println(suma(15,7));
+  }
+
+  public static int suma(int a, int b){
+    return a+b;
+  }
+
 }
 
