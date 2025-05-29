bba86a07caa6cacf90333e6050fe928f92d90089
José Roberto Mociño Morales <99136589+RobertoMoralesmomr@users.noreply.github.com>
2025-05-29 03:14:11 +0000

    e añade el método privado de resta 9.14 pm

--- claseMain.java
+++ claseMain.java
@@ -1,13 +1,18 @@
 public class claseMain{
   public static void main (String[]args){
-    System.out.println("Hols perros desde mi iPad");
+    System.out.println("Hola perros desde mi iPad");
 
-    System.out.println(suma(15,7));
+    System.out.println("Suma desde un método publico: "+suma(15,7));
+
+    System.out.println("Resta desde un método privado: "+resta(10,5));
   }
 
   public static int suma(int a, int b){
     return a+b;
   }
 
+  private static int resta(int s, int b){
+    return s-b;
+  }
 }
 
