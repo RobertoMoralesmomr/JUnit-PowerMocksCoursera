f31aa0a8de2bc1ca73236da11eca78d60bdfcc58
José Roberto Mociño Morales <99136589+RobertoMoralesmomr@users.noreply.github.com>
2025-06-19 04:24:05 +0000

    Se agrega una nueva clase para acceder a los metodos  externamente

---
+++ claseMaintTest.java
@@ -0,0 +1,10 @@
+
+public class claseMaintTest{
+
+    public static void main (String []args){
+        claseMain objetoClaseMain = new claseMain();
+
+        
+        System.out.println("Se imprime desde system: "+objetoClaseMain.resta(15,30));
+    }
+}
