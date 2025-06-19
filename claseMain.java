69d43c8bae5a1b0f2c3578d5f8d6c158d9d0e1b6
José Roberto Mociño Morales <99136589+RobertoMoralesmomr@users.noreply.github.com>
2025-06-19 04:27:09 +0000

    Se modifica la clase Main

--- claseMain.java
+++ claseMain.java
@@ -1,11 +1,11 @@
 public class claseMain{
-  public static void main (String[]args){
+  public static void main (String [] args){
     System.out.println("Hola perros desde mi iPad");
 
     System.out.println("Suma desde un método publico: "+suma(15,7));
 
     System.out.println("Resta desde un método privado: "+resta(10,5));
-  }
+  } 
 
   public static int suma(int a, int b){
     return a+b;
